71a8721d607ebab4dff765e8d94b43d1d36c1e15
andersondefaria1980 <andersondefaria1980@gmail.com>
2022-04-26 20:13:50 -0300

    starting section 3

---
+++ node-app/app.js
@@ -0,0 +1,7 @@
+// const utils = require('./utils.js')
+// console.log(utils.name)
+// console.log(utils.add(2,1))
+
+const notes = require('./notes.js')
+
+console.log(notes.getNotes())
---
+++ node-app/utils.js
@@ -0,0 +1,9 @@
+console.log('utils.js')
+
+const name = 'Anderson'
+
+const add = function (a, b){
+    return a + b
+}
+
+module.exports = {name, add}
