6a12bd8b5594d40a27f650932e238ca42182b6fc
andersondefaria1980 <andersondefaria1980@gmail.com>
2022-11-02 17:46:30 -0300

    finishing section 7

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -1,20 +1,21 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
-
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine','hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
-
 
 app.get('', (req, res) => {
     res.render('index', {
@@ -44,6 +45,22 @@ app.get('/weather', (req, res) => {
         forecast: 'Sunny! It is currently 30 degrees out. It feels like 33 degrees.',
         location: 'Florianópolis'
     })
+})
+
+app.get('/help/*', (req, res) => {
+	res.render('404', {
+		title: 'Not Found',
+		message: 'Help article nor found.',
+		name: 'Anderson Faria',
+	})
+})
+
+app.get('*', (req, res) => {
+	res.render('404', {
+		title: 'Not Found',
+		message: 'Page not found.',
+		name: 'Anderson Faria',
+	})
 })
 
 
